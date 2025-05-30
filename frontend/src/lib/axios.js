2dea642f57ee5e1bdb64a2e588733352b6f94c35
Joy Abdin Sanai <zjoy.311@gmail.com>
2025-05-30 23:10:22 +0530

    Base Url Changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  // baseURL: "http://localhost:5001/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   // baseURL: import.meta.env.VITE_API_BASE_URL,
   // baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   withCredentials: true,
